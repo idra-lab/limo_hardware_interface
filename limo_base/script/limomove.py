87408b3271eccea9957cd5b4702ad0910d8083a5
Michele Focchi <michele.focchi@unitn.it>
2025-09-19 17:15:50 +0000

    added namespace

--- limo_base/script/limomove.py
+++ limo_base/script/limomove.py
@@ -3,10 +3,10 @@
 from pylimo import limo
 import time
 limo=limo.LIMO()
-limo.EnableCommand()         # 使能控制s
+limo.EnableCommand()
 num=10
 while num>0:
 
-    limo.SetMotionCommand(linear_vel=0.5,angular_vel=0)
+    limo.SetMotionCommand(linear_vel=1.,angular_vel=0)
     time.sleep(0.3)
     num-=1
